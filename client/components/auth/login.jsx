fd0c11a4ba89831fb92fc4e34016deab6dba1a16
vVitalv <vitalvital@bk.ru>
2022-05-10 17:14:58 +0300

    auth form

--- client/components/auth/login.jsx
+++ client/components/auth/login.jsx
@@ -3,15 +3,18 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { updateLoginField, updatePasswordField, signIn, signUp } from '../../redux/reducers/auth'
 
+const MIN_PASSWORD_LENGTH = 5
+const MAX_PASSWORD_LENGTH = 20
+const MAX_USERNAME_LENGTH = 20
+
 const LoginForm = () => {
   const dispatch = useDispatch()
-  const { username, password, loginErrMessage } = useSelector((store) => store.auth)
-
+  const { login, password, loginErrMessage } = useSelector((store) => store.auth)
   return (
     <main className="flex flex-col w-full h-screen justify-center items-center bg-slate-400">
       <div className="w-min bg-slate-500 p-4 rounded-md shadow-lg shadow-amber-300">
-        <form className="w-min font-bold text-gray-800">
-          <div className="flex flex-col gap-y-3 w-56">
+        <form className="w-min font-mono font-semibold text-gray-800">
+          <div className="flex flex-col gap-y-3 w-60">
             <div className="flex flex-col gap-y-1">
               <label className="self-end" htmlFor="nickname">
                 Nickname
@@ -20,27 +23,27 @@ const LoginForm = () => {
                 <svg className="h-6" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                   <path
                     stroke="black"
-                    fill="#fbbf24"
+                    fill={login ? '#84cc16' : '#fbbf24'}
                     d="M 25 90
-                  A 45 45, 0, 1, 1, 75 90
-                  A 12 16, -40, 0, 0, 57 70
-                  v -5
-                  A 15 17, 0, 1, 0, 43 65
-                  v 5
-                  A 16 12, -60, 0, 0, 25 90
-                  z
-                  "
+                    A 45 45, 0, 1, 1, 75 90
+                    A 12 16, -40, 0, 0, 57 70
+                    v -5
+                    A 15 17, 0, 1, 0, 43 65
+                    v 5
+                    A 16 12, -60, 0, 0, 25 90
+                    z
+                    "
                   />
                 </svg>
                 <input
                   className="bg-amber-100 px-2 rounded-sm outline-none"
                   id="nickname"
-                  value={username}
+                  value={login}
                   onChange={(e) => {
                     dispatch(updateLoginField(e.target.value))
                   }}
                   type="text"
-                  maxLength={20}
+                  maxLength={MAX_USERNAME_LENGTH}
                   placeholder="Nickname"
                 />
               </div>
@@ -53,16 +56,22 @@ const LoginForm = () => {
                 <svg className="h-6" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                   <path
                     stroke="black"
-                    fill="#fbbf24"
+                    fill={password.length >= MIN_PASSWORD_LENGTH ? '#84cc16' : '#fbbf24'}
                     d="M 25 90
-                  A 45 45, 0, 1, 1, 75 90
-                  A 12 16, -40, 0, 0, 57 70
-                  v -5
-                  A 15 17, 0, 1, 0, 43 65
-                  v 5
-                  A 16 12, -60, 0, 0, 25 90
-                  z
-                  "
+                    A 45 45, 0, 1, 1, 75 90
+                    v -30
+                    h -5
+                    v -10
+                    A 1 1, 0, 1, 0, 30 50
+                    v 10
+                    h -5
+                    z
+                    M 38 60
+                    h 24
+                    v -10
+                    A 1 1, 0, 1, 0, 38 50
+                    z
+                    "
                   />
                 </svg>
                 <input
@@ -73,14 +82,15 @@ const LoginForm = () => {
                     dispatch(updatePasswordField(e.target.value))
                   }}
                   type="password"
-                  maxLength={20}
-                  placeholder="******************"
+                  minLength={MIN_PASSWORD_LENGTH}
+                  maxLength={MAX_PASSWORD_LENGTH}
+                  placeholder="**********"
                 />
               </div>
             </div>
-            <div className="flex justify-around">
+            <div className="flex justify-around my-2">
               <button
-                className="border border-gray-300 rounded-md px-4 py-1"
+                className="bg-slate-400 font-semibold text-xs rounded-md px-4 py-1 transition-colors duration-200 hover:text-amber-200"
                 type="button"
                 onClick={() => {
                   dispatch(signIn())
@@ -90,7 +100,7 @@ const LoginForm = () => {
               </button>
               <p className="p-1">or</p>
               <button
-                className="border border-gray-300 rounded-md px-4 py-1"
+                className="bg-slate-400 font-semibold text-xs rounded-md px-4 py-1 transition-colors duration-200 hover:text-amber-200"
                 type="button"
                 onClick={() => {
                   dispatch(signUp())
@@ -109,4 +119,5 @@ const LoginForm = () => {
   )
 }
 
-export default LoginForm
+LoginForm.propTypes = {}
+export default React.memo(LoginForm)
